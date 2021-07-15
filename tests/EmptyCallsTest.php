311df377f7de77aa5928156c80c47ed5a0a9ac9f
Michal Špaček <mail@michalspacek.cz>
2021-07-15 06:36:29 +0200

    Add support for `empty` language construct too
    Because sometimes you just feel empty and it's better to detect is soon 🤗

---
+++ tests/EmptyCallsTest.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types = 1);
+
+namespace Spaze\PHPStan\Rules\Disallowed;
+
+use PHPStan\File\FileHelper as PHPStanFileHelper;
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+
+class EmptyCallsTest extends RuleTestCase
+{
+
+	protected function getRule(): Rule
+	{
+		return new EmptyCalls(
+			new DisallowedHelper(new FileHelper(new PHPStanFileHelper(__DIR__))),
+			[
+				[
+					'function' => 'empty()',
+					'allowIn' => [
+						'src/disallowed-allowed/*.php',
+						'src/*-allow/*.*',
+					],
+				],
+			]
+		);
+	}
+
+
+	public function testRule(): void
+	{
+		// Based on the configuration above, in this file:
+		$this->analyse([__DIR__ . '/src/disallowed/functionCalls.php'], [
+			[
+				'Calling empty() is forbidden, because reasons',
+				41,
+			],
+		]);
+		// Based on the configuration above, no errors in this file:
+		$this->analyse([__DIR__ . '/src/disallowed-allow/functionCalls.php'], []);
+	}
+
+}
