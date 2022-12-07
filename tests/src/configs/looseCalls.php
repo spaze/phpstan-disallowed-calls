c3ffa8b7de719de33f14fdba2e0f91eecc3bc11f
Michal Špaček <mail@michalspacek.cz>
2022-12-07 04:20:11 +0100

    Update loose config to use allowParamFlagsAnywhere
    Followup to #150

--- tests/src/configs/looseCalls.php
+++ tests/src/configs/looseCalls.php
@@ -7,4 +7,7 @@ in_array('foo', [], false);
 htmlspecialchars('foo');
 htmlspecialchars('foo', ENT_QUOTES);
 htmlspecialchars('foo', 3);
+htmlspecialchars('foo', ENT_QUOTES | ENT_HTML5);
+htmlspecialchars('foo', 51);  // ENT_QUOTES | ENT_HTML5
+htmlspecialchars('foo', ENT_XHTML);
 htmlspecialchars('foo', 4);
