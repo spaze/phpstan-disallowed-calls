66e6ccbb8ed256e9873c8a1ccbf3ad1723eb6ec8
Michal Špaček <mail@michalspacek.cz>
2023-07-01 23:17:33 +0200

    Correct allowed/disallowed comments in test subject with attributes

--- tests/src/disallowed-allow/ClassWithAttributesAllow.php
+++ tests/src/disallowed-allow/ClassWithAttributesAllow.php
@@ -5,23 +5,23 @@ namespace Attributes;
 
 use Waldo\Quux\Blade;
 
-#[\Attributes\AttributeEntity] // disallowed, no $repositoryClass parameter specified
+#[\Attributes\AttributeEntity] // allowed by path in AttributeUsagesTest, disallowed in AttributeUsagesAllowParamsMultipleTest because no $repositoryClass parameter specified
 class ClassWithAttributesAllow
 {
 
-	#[\Attributes\AttributeEntity(repositoryClass: \Attributes\UserRepository::class, readOnly: false)] // disallowed, $repositoryClass present with any value
+	#[\Attributes\AttributeEntity(repositoryClass: \Attributes\UserRepository::class, readOnly: false)] // allowed by path in AttributeUsagesTest, disallowed in AttributeUsagesAllowParamsMultipleTest because $repositoryClass has other value
 	public function hasAvocado(): bool
 	{
 	}
 
 
-	#[\Attributes\AttributeEntity(\Attributes\UserRepository::class)] // allowed, $repositoryClass present with any value
+	#[\Attributes\AttributeEntity(\Attributes\UserRepository::class)] // allowed by path in AttributeUsagesTest, disallowed in AttributeUsagesAllowParamsMultipleTest because $repositoryClass has other value
 	public function hasTuna(): bool
 	{
 	}
 
 
-	#[\Attributes\AttributeEntity(Blade::class)] // allowed, $repositoryClass present with any value
+	#[\Attributes\AttributeEntity(Blade::class)] // allowed in all tests, $repositoryClass present with allowed value
 	public function hasKetchup(): bool
 	{
 	}
