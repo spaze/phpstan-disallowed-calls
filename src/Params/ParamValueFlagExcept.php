ad496c758024757351424d19ab9bb7278a118383
Michal Špaček <mail@michalspacek.cz>
2024-10-08 03:58:36 +0200

    Remove `@throws` exceptions that are not thrown in the method

--- src/Params/ParamValueFlagExcept.php
+++ src/Params/ParamValueFlagExcept.php
@@ -5,14 +5,12 @@ namespace Spaze\PHPStan\Rules\Disallowed\Params;
 
 use PHPStan\Type\Type;
 use Spaze\PHPStan\Rules\Disallowed\Exceptions\UnsupportedParamTypeException;
-use Spaze\PHPStan\Rules\Disallowed\Exceptions\UnsupportedParamTypeInConfigException;
 
 class ParamValueFlagExcept extends ParamValueFlag
 {
 
 	/**
 	 * @throws UnsupportedParamTypeException
-	 * @throws UnsupportedParamTypeInConfigException
 	 */
 	public function matches(Type $type): bool
 	{
